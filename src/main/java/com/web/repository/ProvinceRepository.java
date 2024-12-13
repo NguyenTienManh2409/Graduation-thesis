952e7aed999a46d604e270af80eef706e35da177
NguyenTienManh2409 <21020777@vnu.edu.vn>
2024-12-13 18:24:40 +0700

    Revert "Update"
    This reverts commit c11019508c1d96cbed435e65a597538ab46a3a2b.

--- src/main/java/com/web/repository/ProvinceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.web.repository;
-
-import com.web.entity.Category;
-import com.web.entity.Province;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProvinceRepository extends JpaRepository<Province,Long> {
-}
